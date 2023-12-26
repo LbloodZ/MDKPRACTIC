3bd39432a7b19edecc5f3115f3a6f39576545a87
LbloodZ <154913560+LbloodZ@users.noreply.github.com>
2023-12-27 00:04:16 +0700

    Добавление привязки

---
+++ ПРИВЯЗКА/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Click(object sender, RoutedEventArgs e)
+        {
+            if (txt2.Text != "")
+            {
+                MessageBox.Show(txt2.Text, "Ваше имя", MessageBoxButton.OK,MessageBoxImage.Information);
+            }
+            else
+            {
+                MessageBox.Show("Ошибка", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Text files (*.txt)|*.txt|All (*.*)|*.*";
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                File.WriteAllText(saveFileDialog.FileName, txt1.Text.ToString());
+            }
+        }
+
+        private void Button_Click2(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.Filter = "Image files (*.png, *.jpg)|*.png;*.jpg|Все файлы (*.*)|*.*";
+            if (openFile.ShowDialog() == true)
+            {
+                string fileName = openFile.FileName;
+                img.Source = new BitmapImage(new Uri(fileName));
+            }
+        }
+    }
+}
